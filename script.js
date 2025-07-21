b5891ec74d46de66389dc586beb93d0175de1217
Willem April <willemapril33@gmail.com>
2025-07-21 21:20:33 +0200

    Add testimonials slider and images to homepage
    Introduced a testimonials section with a Swiper.js slider, including five new user images and testimonial content. Updated index.html to include the new section and Swiper.js integration, added corresponding styles in style.css, and initialized the slider in script.js.

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ menuButton.addEventListener("click", () => {
         menuButton.classList.remove("fa-times")
     }
 })
+
+const swiper = new Swiper(".slider-wrapper", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+})
